f98b7cf430a143e6da725e20113d15146a7e5a68
olha <yakowenko.olya@gmail.com>
2023-11-12 10:00:04 +0300

    commit a file

---
+++ bubblesort.java
@@ -0,0 +1,11 @@
+public int[] bubblesort (int[] list) {
+    for (i=0; i<list.length; i++) {
+      for (k=0; k<list.length-1; i++) {
+        if (list[k] > list[k+1]) {
+          storage = list[k];
+          list[k] = list[k+1];
+          list[k+1] = storage;
+        }
+      }
+    }
+  
